0d3eba901a54ae7f42759ea5525013683f732999
Jahnke <naterased@gmail.com>
2022-04-11 16:23:36 -0500

    created slug, need to style next.

--- pages/fish/[slug].js
+++ pages/fish/[slug].js
@@ -1,19 +1,77 @@
-import { sanityClient, urlFor, usePreviewSubscription,PortableText,
- } from "../../lib/sanity";
+import {
+    sanityClient,
+    urlFor,
+    usePreviewSubscription,
+    PortableText,
+} from "../../lib/sanity";
 
-const fishQuery = ``
+const fishQuery = `*[_type == "fish" && slug.current == $slug][0]{
+    _id,
+    name,
+    scientificName,
+    slug,
+    mainImage,
+    length,
+    weight,
+    description,
+}`
 
 
 
 
-export default function OneFish() {
-
+export default function OneFish({ data }) {
+    const { fish } = data;
+    return (
+        // <article>
+        //     <h1>{fish.name}</h1>
+        //     <main>
+        //         <img />
+        //         <div>
+        //             <ul>
+        //                 <li></li>
+        //             </ul>
+        //             <h3>Here</h3>
+        //         </div>
+        //     </main>
+        // </article>
+        <div className="rounded-lg shadow-lg mx-4 mt-2 bg-[#edf6f9] h-screen">
+            <div className="">
+                <img className="p-2 rounded-lg bg-[#4BB6EF] shadow" src={urlFor(fish.mainImage).url()} alt="" />
+            </div>
+            <div className="px-4 mt-2">
+                <h3 className="text-lg font-semibold text-gray-800">{fish.name}</h3>
+                <p className="text-gray-500 italic text-sm pb-2">{fish.scientificName}</p>
+                <div className="flex">
+                    <img className="pb-2" src="/ruler.png" />
+                    <p className="self-center pl-2">to {fish.length} in.</p>
+                </div>
+                <div className="flex">
+                    <img className="pb-2" src="/scale.png" />
+                    <p className="self-center pl-2">to {fish.weight} lbs.</p>
+                </div>
+                <p>{fish.description}.</p>
+            </div>
+        </div>
+    )
 }
 
 export async function getStaticPaths() {
+    const paths = await sanityClient.fetch(
+        `*[_type == "fish" && defined(slug.current)]{
+        "params": {
+          "slug": slug.current
+        }
+      }`
+    );
 
+    return {
+        paths,
+        fallback: true,
+    };
 }
 
-export async function getStaticProps() {
-
+export async function getStaticProps({ params }) {
+    const { slug } = params;
+    const fish = await sanityClient.fetch(fishQuery, { slug });
+    return { props: { data: { fish }, preview: true } };
 }
--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ const fishQuery = `*[_type == "fish"]{
   weight,
 }`;
 
-export default function Home({fish}) {
-  return ( 
+export default function Home({ fish }) {
+  return (
     // <div classNameName = "flex space-x-2 justify-center" >
     // <button type = "button" classNameName = "inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">{fish[0].name}</button>
     // <div className="rounded-lg shadow-lg mx-4 mt-2 bg-[#edf6f9]">
@@ -44,29 +44,33 @@ export default function Home({fish}) {
     //         </div>
     //     </div> 
 
-      <div>
-        <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
-          {fish?.length > 0 && fish.map((fish) => (
+    <div>
+      <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
+        {fish?.length > 0 && fish.map((fish) => (
           <li key={fish._id} className="rounded-lg shadow-lg mx-4 mt-2 bg-[#4bb6ef62]">
-              <div className="h-48 bg-[#4BB6EF] relative rounded-lg shadow-lg">
-              <Image src={urlFor(fish.mainImage).url()} layout="fill" objectFit="contain" className="p-2 rounded-lg shadow relative" />
+            <div className="h-48 bg-[#4BB6EF] relative rounded-lg shadow-lg">
+              <Link href={`/fish/${fish.slug.current}`}>
+                <a>
+                  <Image src={urlFor(fish.mainImage).url()} layout="fill" objectFit="contain" className="p-2 rounded-lg shadow relative" />
+                </a>
+              </Link>
+            </div>
+            <div className="p-4 h-48">
+              <h3 className="text-xl font-semibold text-gray-800">{fish.name}</h3>
+              <p className="text-gray-500 italic text-sm pb-2">{fish.scientificName}</p>
+              <div className="flex">
+                <img className="pb-2" src="/ruler.png" />
+                <p className="self-center pl-2">to {fish.length} in.</p>
               </div>
-              <div className="p-4 h-48">
-                <h3 className="text-xl font-semibold text-gray-800">{fish.name}</h3>
-                <p className="text-gray-500 italic text-sm pb-2">{fish.scientificName}</p>
-                <div className="flex">
-                    <img className="pb-2" src="/ruler.png" />
-                    <p className="self-center pl-2">to {fish.length} in.</p>
-                </div>
-                <div className="flex">
-                    <img className="pb-2" src="/scale.png" />
-                    <p className="self-center pl-2">to {fish.weight} lbs.</p>
-                </div>
+              <div className="flex">
+                <img className="pb-2" src="/scale.png" />
+                <p className="self-center pl-2">to {fish.weight} lbs.</p>
               </div>
+            </div>
           </li>
-          ))}
-        </ul>
-      </div>      
+        ))}
+      </ul>
+    </div>
   );
 }
 
